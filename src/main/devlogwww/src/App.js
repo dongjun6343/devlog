3a36b9e5e26955c1065ad72afbd37b7bf5d8f834
dongjun6343 <dongjun6343@naver.com>
2023-02-15 19:15:36 +0900

    값을 입력받는 기능 추가

--- src/main/devlogwww/src/App.js
+++ src/main/devlogwww/src/App.js
@@ -1,5 +1,6 @@
 //import axios from 'axios';
 import React from 'react';
+import './App.css';
 import Home from './pages/Home';
 
 // 추후에 기능이 고도화되면 라우팅하여 여러개의 페이지를 관리
--- src/main/devlogwww/src/components/InputBox.jsx
+++ src/main/devlogwww/src/components/InputBox.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
-const InputBox = () => (
-    <div className="todoapp__inputbox">
-        <input
-            type="text"
-            name="todoItem"
-            placeholder="ToDo 입력!!! "
-            className="todoapp__inputbox-inp"
-        />
-        <button type="submit" className="todoapp__inputbox-add-btn">
-            추가
-        </button>
-    </div>
-);
+const InputBox = () => {
+
+    // 빈문자열 text와 setText()를 생성한다.
+    const [text, setText] = useState('');
+
+    // Hook으로 ref 생성
+    const inputRef = useRef(null);
+
+    // input값이 변할때 e.target에 있는 <input ... /> 으로부터 value 값을 가져오는 메소드
+    const onChangeInput = (e) => {
+
+        setText(e.target.value);
+    };
+
+    const onClickAddButton = () => {
+        setText(''); // 초기화.
+        inputRef.current.focus(); //
+    }
+
+    return (
+
+        <div className="todoapp__inputbox">
+            <input
+                type="text"
+                name="todoItem"
+                value={text}
+                ref={inputRef} // 생성한 ref 할당
+                placeholder="오늘 할 일을 입력하세요. "
+                className="todoapp__inputbox-inp"
+                onChange={onChangeInput} // input이 변하는 이벤트가 발생했을때, onChangeInput() 실행
+            />
+
+            <button
+                type="submit"
+                className="todoapp__inputbox-add-btn"
+                onClick={onClickAddButton} >
+                추가
+            </button>
+        </div>
+    );
+};
 
 export default InputBox;
