98ff3294e6ec9ce4540d7a1791818bb6a2dbbb05
dongjun6343 <dongjun6343@naver.com>
2023-02-15 20:42:13 +0900

    todolist 수정하기 추가

--- src/main/devlogwww/src/components/ToDoItem.jsx
+++ src/main/devlogwww/src/components/ToDoItem.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
 const ToDoItem = ({todoItem, todoList, setTodoList}) => {
+
+    const [edited, setEdited] = useState(false); // 수정모드인지 플래그 값 확인
+
+    const [newText, setNewTest] = useState(todoItem.text); // 새로운 아이템 내용
+
+    const editInputRef = useRef(null);
+
+    useEffect(() => {
+        // edit 모드일때 포커싱을 한다.
+        if (edited) {
+            editInputRef.current.focus();
+        }
+    }, [edited]);
 
     const onChangeCheckbox = () => {
 
@@ -11,9 +23,28 @@ const ToDoItem = ({todoItem, todoList, setTodoList}) => {
 
         setTodoList(nextTodoList);
     };
+    
+    const onClickEditButton = () => {
+        setEdited(true);
+    }
+
+    // 수정모드
+    const onChangeEditInput = (e) => {
+        setNewTest(e.target.value);
+    };
+
+    const onClickSubmitButton = () => {
+        const nextTodoList = todoList.map((item) => ({
+            ...item,
+            text: item.id === todoItem.id ? newText : item.text, // 새로운 아이템 내용을 넣어줌
+        }));
+        setTodoList(nextTodoList); // 새로운 리스트를 넣어줌
+
+        setEdited(false); // 수정모드를 다시 읽기모드로 변경
+    };
+    
 
     return (
-
         <li className="todoapp__item">
             <input type="checkbox"
                    className="todoapp__item-checkbox"
@@ -21,21 +52,48 @@ const ToDoItem = ({todoItem, todoList, setTodoList}) => {
                    onChange={onChangeCheckbox}
             />
 
-            <span className={`todoapp__item-ctx ${
-                todoItem.checked ? 'todoapp__item-ctx-checked' : ''
-            }`}
-            >
-
-                {todoItem.text}
-
-            </span>
+            {
+                // 아이템 내용
+                edited ? (
+                    <input
+                        type="text"
+                        className="todoapp__item-edit-input"
+                        value={newText}
+                        ref={editInputRef}
+                        onChange={onChangeEditInput}
+                    />
+                ) : (
+                    <span
+                        className={`todoapp__item-ctx ${
+                            todoItem.checked ? 'todoapp__item-ctx-checked' : ''
+                        }`}
+                    >
+            {todoItem.text}
+          </span>
+                )
+            }
+            
             {
                 // 수정 버튼
-                // 완료한 일인 경우에는 null을 반환하여 보이지 않도록 한다.
+                // 완료한 일인 경우에는 null을 반환하여 보이지 않도록 함
                 !todoItem.checked ? (
-                    <button type="button" className="todoapp__item-edit-btn">
-                        수정
-                    </button>
+                    edited ? (
+                        <button
+                            type="button"
+                            className="todoapp__item-edit-btn"
+                            onClick={onClickSubmitButton}
+                        >
+                            수정모드
+                        </button>
+                    ) : (
+                        <button
+                            type="button"
+                            className="todoapp__item-edit-btn"
+                            onClick={onClickEditButton}
+                        >
+                            수정
+                        </button>
+                    )
                 ) : null
             }
 
